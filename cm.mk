26552580f402453418ddf9c278dc7b752c22eccf
Kra1o5 <kra1o5x@gmail.com>
2014-05-08 01:05:38 +0200

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,19 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 600
+TARGET_SCREEN_WIDTH := 1024
+
+# Inherit some common CyanogenMod stuff
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/bq/maxwell2/full_maxwell2.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := maxwell2
+PRODUCT_NAME := cm_maxwell2
+PRODUCT_BRAND := bq
+PRODUCT_MODEL := bq Maxwell 2
+PRODUCT_MANUFACTURER := Rockchip
+
+# Set product name
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=bq_Maxwell2
---
+++ device.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_AAPT_CONFIG := normal mdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+DEVICE_PACKAGE_OVERLAYS += device/bq/maxwell2/overlay
+
+# Hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
+
+# Config files
+PRODUCT_COPY_FILES += \
+    device/bq/maxwell2/config/media_profiles.xml:system/etc/media_profiles.xml
+
+# Rootdir blob
+PRODUCT_COPY_FILES += \
+    device/bq/maxwell2/rootdir/rk30xxnand_ko.ko.3.0.36+:root/rk30xxnand_ko.ko.3.0.36+
+
+# Prebuilt
+PRODUCT_COPY_FILES += \
+    device/bq/maxwell2/prebuilt/gps.conf:system/etc/gps.conf \
+    device/bq/maxwell2/prebuilt/gpsconfig.xml:system/etc/gps/gpsconfig.xml \
+    device/bq/maxwell2/prebuilt/updater:obj/EXECUTABLES/updater_intermediates/updater
+	
+# Default config
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=160 \
+    ro.audio.flinger_standbytime_ms=1000
